aa33d5284f039b68b971e4a6c62522d30e6ad1ac
Ajwwkxkdang <107187521+Ajwyunsx@users.noreply.github.com>
2024-03-10 17:32:44 +0800

    Add files via upload

--- assets/preload/stages/turmoilsweep.hx
+++ assets/preload/stages/turmoilsweep.hx
@@ -1,27 +1,31 @@
+var fartree;
+var backtree;
+var lashojas;
+var ramasnose;
 function onCreate()
 {
-var fartree:FlxSprite = new FlxSprite(-1300, -750).loadGraphic(Paths.image('mario/Turmoil/ThirdBGTrees'));
+ fartree = new FlxSprite(-1300, -750).loadGraphic(Paths.image('mario/Turmoil/ThirdBGTrees'));
 				fartree.scale.set(3.5, 3.5);
 				fartree.updateHitbox();
-				add(fartree);
+				addBehindGF(fartree);
 
-				var backtree:FlxSprite = new FlxSprite(-1300, -750).loadGraphic(Paths.image('mario/Turmoil/SecondBGTrees'));
+				backtree = new FlxSprite(-1300, -750).loadGraphic(Paths.image('mario/Turmoil/SecondBGTrees'));
 				backtree.scale.set(3.5, 3.5);
 				backtree.updateHitbox();
-				add(backtree);
+				addBehindGF(backtree);
 
-				var floor:FlxSprite = new FlxSprite(-130, -750).loadGraphic(Paths.image('mario/Turmoil/MainFloorAndTrees'));
+				floor = new FlxSprite(-1300, -750).loadGraphic(Paths.image('mario/Turmoil/MainFloorAndTrees'));
 				floor.scale.set(3.35, 3.35);
 				floor.updateHitbox();
-				add(floor);
+				addBehindGF(floor);
 
-				var lashojas:FlxSprite = new FlxSprite(-1300, -350).loadGraphic(Paths.image('mario/Turmoil/TreeLeaves'));
+				lashojas = new FlxSprite(-1300, -350).loadGraphic(Paths.image('mario/Turmoil/TreeLeaves'));
 				lashojas.scale.set(3.5, 3.5);
 				lashojas.updateHitbox();
-				add(lashojas);
+				addBehindGF(lashojas);
 
-				var ramasnose:FlxSprite = new FlxSprite(-1300, -350).loadGraphic(Paths.image('mario/Turmoil/TreesForeground'));
+				ramasnose = new FlxSprite(-1300, -350).loadGraphic(Paths.image('mario/Turmoil/TreesForeground'));
 				ramasnose.scale.set(3.35, 3.35);
 				ramasnose.updateHitbox();
-				add(ramasnose);
+				addBehindGF(ramasnose);
 }
