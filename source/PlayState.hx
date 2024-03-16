eb03827b3cd6a4f5f319b2433a466566726f38b2
Ajwwkxkdang <107187521+Ajwyunsx@users.noreply.github.com>
2024-03-16 23:22:54 +0800

    Add files via upload

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1902,35 +1902,14 @@ class PlayState extends MusicBeatState
 					noHUD = true;
 					specialGameOver = true;
 					
-					
-				var scarymansion:BGSprite = new BGSprite('mario/LuigiBeta/BackBG', -1200, -850, 1.8, 1.8);
-					scarymansion.antialiasing = ClientPrefs.globalAntialiasing;
-					add(scarymansion);
-
-					add(gfGroup);
-
-					var betafire1:BGSprite = new BGSprite('mario/LuigiBeta/Alone_Fire', -320, -630, ['fire'], true);
-					//betafire.setGraphicSize(Std.int(lluvia.width * 1.7));
-					betafire1.antialiasing = ClientPrefs.globalAntialiasing;
-					add(betafire1);
-
-					var betafire2:BGSprite = new BGSprite('mario/LuigiBeta/Alone_Fire', 1270, -630, ['fire'], true);
-					//betafire.setGraphicSize(Std.int(lluvia.width * 1.7));
-					betafire2.antialiasing = ClientPrefs.globalAntialiasing;
-					betafire2.flipX = true;
-					add(betafire2);
-
-					var scaryfloor:BGSprite = new BGSprite('mario/LuigiBeta/FrontBG', -1200, -850);
-					scaryfloor.antialiasing = ClientPrefs.globalAntialiasing;
-					scaryfloor.scale.x = 2.0;
-					scaryfloor.scale.y = 2.0;
-					add(scaryfloor);
 
 					starmanGF = new BGSprite('characters/Beta_Luigi_GF_Assets', 570, 100, 2, 2, ["GFIdle"], false);
 					starmanGF.animation.addByIndices('danceRight', 'GFIdle', [15,16,17,18,19,20,21,22,23,24,25,26,27,28,29], "", 24, false);
 					starmanGF.animation.addByIndices('danceLeft', 'GFIdle', [30,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14], "", 24, false);
 					starmanGF.animation.addByPrefix('sad', "GFMiss", 24, false);
 					starmanGF.antialiasing = ClientPrefs.globalAntialiasing;
+					starmanGF.scale.x = 2.0;
+					starmanGF.scale.y = 2.0;
 					add(starmanGF);
 
 					lluvia = new BGSprite('mario/LuigiBeta/old/Beta_Luigi_Rain_V1', -170, 50, ['RainLuigi'], true);
@@ -17286,8 +17265,8 @@ class PlayState extends MusicBeatState
 			script.setVariable("ClientPrefs", ClientPrefs);
 			script.setVariable("FlxTimer", FlxTimer);
 			script.setVariable("Main", Main);
-			script.setVariable("Note", Note);
-			//script.setVariable("note", note);
+			//script.setVariable("Note", Note);
+			script.setVariable("note", Note);
 			script.setVariable("Conductor", Conductor);
 			script.setVariable("Std", Std);
 			script.setVariable("FlxTextBorderStyle", FlxTextBorderStyle);
